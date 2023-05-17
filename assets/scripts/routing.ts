2c7b2c7dbd77984cbf92bfc98abc5e69be77b3bf
Alex Seifert <alex@alexseifert.com>
2023-05-17 21:45:22 +0200

    Refactor

--- assets/scripts/routing.ts
+++ assets/scripts/routing.ts
@@ -1,50 +1,53 @@
-function setLinks() {
+async function clickLink(link: HTMLAnchorElement): Promise<void> {
+  const pageLoader = document.getElementById('pageLoader');
+
+  if (pageLoader) {
+    pageLoader.classList.remove('hidden');
+    pageLoader.classList.add('loading');
+  }
+
+  try {
+    const response = await fetch(`${link.href}?_partial`);
+
+    if (response.status === 200) {
+      const partialContents = await response.json();
+      const main = document.getElementById('main');
+
+      if (main) {
+        main.innerHTML = partialContents.html;
+      }
+
+      document.title = partialContents.title || 'Hybrid Frameworkless SPA Concept';
+
+      window.history.pushState({
+        html: partialContents.html,
+        title: partialContents.pageTitle,
+      }, '', link.href);
+
+      setLinks();
+    }
+    else {
+      throw new Error('The response code was not 200!');
+    }
+  }
+  catch (error: any) {
+    console.error(error);
+    document.location = link.href;
+  }
+  finally {
+    if (pageLoader) {
+      pageLoader.classList.add('hidden');
+      pageLoader.classList.remove('loading');
+    }
+  }
+}
+
+function setLinks(): void {
   const links = document.querySelectorAll<HTMLAnchorElement>('a[data-link]');
 
   links.forEach(link => link.addEventListener('click', async (e: Event) => {
     e.preventDefault();
-
-    const pageLoader = document.getElementById('pageLoader');
-
-    if (pageLoader) {
-      pageLoader.classList.remove('hidden');
-      pageLoader.classList.add('loading');
-    }
-
-    try {
-      const response = await fetch(`${link.href}?_partial`);
-
-      if (response.status === 200) {
-        const partialContents = await response.json();
-        const main = document.getElementById('main');
-
-        if (main) {
-          main.innerHTML = partialContents.html;
-        }
-
-        document.title = partialContents.title || 'Hybrid Frameworkless SPA Concept';
-
-        window.history.pushState({
-          html: partialContents.html,
-          title: partialContents.pageTitle,
-        }, '', link.href);
-
-        setLinks();
-      }
-      else {
-        throw new Error('The response code was not 200!');
-      }
-    }
-    catch (error: any) {
-      console.error(error);
-      document.location = link.href;
-    }
-    finally {
-      if (pageLoader) {
-        pageLoader.classList.add('hidden');
-        pageLoader.classList.remove('loading');
-      }
-    }
+    await clickLink(link);
   }));
 }
 
