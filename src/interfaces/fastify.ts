03c14147419dab5545b0665135b93f25acd3de53
Alex Seifert <alex@alexseifert.com>
2023-05-17 13:57:37 +0200

    Add the first route and full page renderer

---
+++ src/interfaces/fastify.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance, FastifyReply } from 'fastify';
+
+// Copied from node_modules/@fastify/types/index.d.ts
+interface RouteSpecificOptions {
+  layout?: string;
+}
+
+// Temporary workaround for https://github.com/fastify/point-of-view/issues/301
+export interface FastifyReplyWithView extends FastifyReply {
+  view: never;
+  renderFullPage<T extends { [key: string]: any }>(page: string, data: T, opts?: RouteSpecificOptions): FastifyReply;
+  renderFullPage(page: string, data?: object, opts?: RouteSpecificOptions): FastifyReply;
+  renderPartial<T extends { [key: string]: any }>(page: string, data: T, opts?: RouteSpecificOptions): FastifyReply;
+  renderPartial(page: string, data?: object, opts?: RouteSpecificOptions): FastifyReply;
+}
+
+// Temporary workaround for https://github.com/fastify/point-of-view/issues/301
+export interface FastifyInstanceWithView extends FastifyInstance {
+  view: never;
+  renderFullPage<T extends { [key: string]: any }>(page: string, data: T, opts?: RouteSpecificOptions): Promise<string>;
+  renderFullPage(page: string, data?: object, opts?: RouteSpecificOptions): Promise<string>;
+  renderPartial<T extends { [key: string]: any }>(page: string, data: T, opts?: RouteSpecificOptions): Promise<string>;
+  renderPartial(page: string, data?: object, opts?: RouteSpecificOptions): Promise<string>;
+}
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { FastifyRequest } from 'fastify';
+
+import { FastifyInstanceWithView, FastifyReplyWithView } from '~/interfaces/fastify';
+
+export default async (app: FastifyInstanceWithView) => {
+  app.get('/', (req: FastifyRequest, reply: FastifyReplyWithView) => {
+    reply.renderFullPage('home.hbs');
+  });
+};
