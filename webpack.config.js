66af1a8b49b55999d04ba09e733e6a87fe2451c0
Alex Seifert <alex@alexseifert.com>
2023-05-17 10:40:07 +0200

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    bootstrap: { import: './assets/scripts/bootstrap.ts', filename: 'scripts/bootstrap.js' },
+    async: { import: './assets/scripts/async.ts', filename: 'scripts/async.js' },
+    important: { import: './assets/scripts/important.ts', filename: 'scripts/important.js' },
+  },
+  output: {
+    filename: 'scripts/[name].js',
+    path: __dirname + '/public/dist',
+    clean: process.env.NODE_ENV !== 'development',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+};
