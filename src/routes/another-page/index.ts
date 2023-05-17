2bbd0224ac003fc04754df7ab8fa731f7b6ef5d4
Alex Seifert <alex@alexseifert.com>
2023-05-17 20:42:20 +0200

    Add the _partial endpoints

--- src/routes/another-page/index.ts
+++ src/routes/another-page/index.ts
@@ -6,4 +6,13 @@ export default async (app: FastifyInstanceWithView) => {
   app.get('/', (req: FastifyRequest, reply: FastifyReplyWithView) => {
     reply.renderFullPage('anotherPage.hbs');
   });
+
+  app.get('/_partial', async (req: FastifyRequest, reply: FastifyReplyWithView) => {
+    const html = await app.renderPartial('anotherPage.hbs');
+
+    reply.send({
+      title: 'Another Page',
+      html,
+    });
+  });
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,13 @@ export default async (app: FastifyInstanceWithView) => {
   app.get('/', (req: FastifyRequest, reply: FastifyReplyWithView) => {
     reply.renderFullPage('home.hbs');
   });
+
+  app.get('/_partial', async (req: FastifyRequest, reply: FastifyReplyWithView) => {
+    const html = await app.renderPartial('home.hbs');
+
+    reply.send({
+      title: 'Home',
+      html,
+    });
+  });
 };
